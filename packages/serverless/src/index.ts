efd10f0b68ed8034f68c97fa99ff18cfb2ab7469
Matthew Read <mattread90@gmail.com>
2023-02-24 17:07:40 +0000

    chore: Delete GCP code and dependencies Prepare for release

--- packages/serverless/src/index.ts
+++ packages/serverless/src/index.ts
@@ -1,7 +1,7 @@
 // https://medium.com/unsplash/named-namespace-imports-7345212bbffb
 import * as AWSLambda from './awslambda';
-import * as GCPFunction from './gcpfunction';
-export { AWSLambda, GCPFunction };
+
+export { AWSLambda };
 
 export { AWSServices } from './awsservices';
 
